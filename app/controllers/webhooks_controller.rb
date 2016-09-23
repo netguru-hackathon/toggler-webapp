8baa7e9394820541da724c5607155bb05ccfad8c
Wojciech Olejnik <wojciech.olejnik@netguru.co>
2016-09-23 13:19:02 +0200

    fix help

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -2,7 +2,7 @@ class WebhooksController < ApplicationController
   def slack
     command = params["text"]
     if command == "help"
-      render text: help_text, status: 200
+      return render text: help_text, status: 200
     end
     # if (command =~ "login")
   end
