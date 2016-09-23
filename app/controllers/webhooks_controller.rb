884146c09fc65a46f9176172a18d14929575e996
olgad <olgadraber@gmail.com>
2016-09-23 13:27:14 +0200

    command parser base

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -1,11 +1,21 @@
 class WebhooksController < ApplicationController
   def slack
-    command = params["text"]
-    if command == "help"
+    @command_args = params["text"].split(" ")
+    case @command_args.first
+    when "help"
       return render text: help_text, status: 200
+    when "login"
+      # login
+    when "start"
+      # start
+    when "stop"
+      # stop
+    when "list"
+      # list
     end
-    # if (command =~ "login")
   end
+
+  private
 
   def help_text
     "Toggler usage: blabla"
