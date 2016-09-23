4db29bb51acfed79cc274d83fff4ccc51580742e
Wojciech Olejnik <wojciech.olejnik@netguru.co>
2016-09-23 13:18:07 +0200

    Add help action

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -1,5 +1,13 @@
 class WebhooksController < ApplicationController
   def slack
-    binding.pry
+    command = params["text"]
+    if command == "help"
+      render text: help_text, status: 200
+    end
+    # if (command =~ "login")
+  end
+
+  def help_text
+    "Toggler usage: blabla"
   end
 end
